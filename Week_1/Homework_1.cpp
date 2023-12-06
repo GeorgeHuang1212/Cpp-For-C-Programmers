35c940c9c4806e2b8d9126182932e87ac20a188b
George Huang <37496426+GeorgeHuang1212@users.noreply.github.com>
2023-12-06 20:22:25 +0800

    Create Homework_1.cpp

---
+++ Week_1/Homework_1.cpp
@@ -0,0 +1,29 @@
+// Convert a C program to C++
+
+#include <iostream>
+#include <vector>
+
+const int N = 40;
+
+void sum(int *p, int n, std::vector<int> d) {
+  *p = 0;
+  for (int i = 0; i < n; ++i)
+    *p = *p + d[i];
+  
+  return;
+}
+
+int main(void) {
+  int accum = 0;
+  std::vector<int> data;
+  data.reserve(N);
+  
+  for (int i = 0; i < N; ++i) {
+    data.push_back(i);
+  }
+  
+  sum(&accum, N, data);
+  std::cout << "sum is " << accum << std::endl;
+  
+  return 0;
+}
